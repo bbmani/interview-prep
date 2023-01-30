2e0df46e6e19f8c64f1e25265e3fd3ddb0005eff
bbmani <balajibharatwajmanikandan>
2023-01-30 16:17:00 -0500

    UPDATE 468: Concise regex commands

--- Cisco/validate_ip.py
+++ Cisco/validate_ip.py
@@ -2,20 +2,21 @@ import re
 def validateIPAddress(queryIP):
     return_value = ""
     
-    pattern_ipv4 = "^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])$"
+    ipv4 = r"(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])"
+    pattern_ipv4 = re.compile(r'^(' + ipv4 + r'\.){3}' + ipv4 + r'$')
     
-    pattern_ipv6 = "^([0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F])\:([0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F])\:([0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F])\:([0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F])\:([0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F])\:([0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F])\:([0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F])\:([0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F][0-9a-fA-F]|[0-9a-fA-F])$"
+    ipv6 = r"[0-9a-fA-F]{1,4}"
+    pattern_ipv6 = re.compile(r'^(' + ipv6 + r'\:){7}' + ipv6 + r'$')
     
-    
-    if re.search(pattern=pattern_ipv4, string=queryIP):
-        return_value = "IPV4"
-    elif re.search(pattern=pattern_ipv6, string=queryIP):
-        return_value = "IPV6"
+    if re.match(pattern=pattern_ipv4, string=queryIP):
+        return_value = "IPv4"
+    elif re.match(pattern=pattern_ipv6, string=queryIP):
+        return_value = "IPv6"
     else:
         return_value = "Neither"
 
     return return_value
     
 if __name__ == "__main__":
-    final_value = validateIPAddress("1.0.1.")
+    final_value = validateIPAddress("12.12.12")
     print(final_value)
